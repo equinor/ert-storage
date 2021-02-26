d29ee787fe9241ea63a8b76394e3fbbe17c95a68
Åsmund Birkeland <aasmund.birkeland@gmail.com>
2021-02-26 10:30:16 +0100

    Create ert-storage entry point

--- ert_storage/__main__.py
+++ ert_storage/__main__.py
@@ -5,5 +5,20 @@ import os
 import sys
 
 
+def main() -> None:
+    os.execv(
+        sys.executable,
+        [
+            sys.executable,
+            "-m",
+            "uvicorn",
+            "ert_storage.app:app",
+            "--reload",
+            "--reload-dir",
+            os.path.dirname(__file__),
+        ],
+    )
+
+
 if __name__ == "__main__":
-    pass
+    main()
