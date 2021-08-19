0a242187846711ec08036753b7a26a0409d49079
Øystein Olai Heggen <ooh@equinor.com>
2021-08-19 15:38:57 +0200

    Add SPE1_ST integrationtest

--- ci/jenkins/testkomodo.sh
+++ ci/jenkins/testkomodo.sh
@@ -12,5 +12,46 @@ install_package () {
 }
 
 start_tests () {
-    pytest -k "not test_typing"
+    pytest -k "not test_typing and not spe1"
+    run_spe1_tests
+}
+
+run_spe1_tests() {
+  pushd tests/data/spe1_st
+
+  echo "Initiating Ert run for Spe1 with new storage enabled..."
+  ert ensemble_experiment --enable-new-storage spe1.ert
+
+  echo "Ert run finished"
+
+  echo "Run the storage server api ..."
+  ert api &
+  ert_api_pid=$!
+  max_sec=60
+  until [ -f storage_server.json ]
+  do
+      if [ "$max_sec" -le 0 ]
+      then
+          echo "ert-storage has not started properly!"
+          exit 1
+      fi
+      sleep 1
+      max_sec=$(( max_sec-1 ))
+  done
+
+  echo "Test for accessing Spe1 data ..."
+  popd
+
+  set +e
+  pytest -vs -k spe1
+
+  status=$?
+  echo "Close the storage server api ... "
+  kill $ert_api_pid
+  wait $ert_api_pid
+  echo "Completed"
+  if [ "$status" -g 0 ]; then
+      exit $status
+  fi
+
 }
