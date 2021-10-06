dd48ad84919171c47b187de2c030159c2284a0d3
Øystein Olai Heggen <ooh@equinor.com>
2021-10-06 13:13:43 +0200

    Adjust spe1_st integration test
    Adjust get_info fixture to align with the new base_service in ERT

--- ci/jenkins/testkomodo.sh
+++ ci/jenkins/testkomodo.sh
@@ -26,34 +26,10 @@ run_spe1_tests() {
 
   echo "Initiating Ert run for Spe1 with new storage enabled..."
   ert ensemble_experiment --enable-new-storage spe1.ert
-
   echo "Ert run finished"
 
-  echo "Run the storage server api ..."
-  ert api &
-  ert_api_pid=$!
-  max_sec=60
-  until [ -f storage_server.json ]
-  do
-      if [ "$max_sec" -le 0 ]
-      then
-          echo "ert-storage has not started properly!"
-          exit 1
-      fi
-      sleep 1
-      max_sec=$(( max_sec-1 ))
-  done
-
   echo "Test for accessing Spe1 data ..."
+  pytest ../../../ -vs -k spe1
+
   popd
-
-  set +e
-  pytest -vs -k spe1
-
-  status=$?
-  echo "Close the storage server api ... "
-  kill $ert_api_pid
-  wait $ert_api_pid
-  echo "Completed"
-  return "$status"
 }
