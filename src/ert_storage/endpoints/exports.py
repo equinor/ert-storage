227c9e8f2c2a181ae72e5fa99f86567f79622f27
Terry Hannant <thannant_github@metafu.net>
2021-11-25 14:52:20 +0100

    Fix minor formatting

--- src/ert_storage/endpoints/exports.py
+++ src/ert_storage/endpoints/exports.py
@@ -5,7 +5,7 @@ from fastapi import APIRouter, Depends, Query, status
 from fastapi.responses import Response
 import pandas as pd
 from pandas.core.frame import DataFrame
-from sqlalchemy import *
+from sqlalchemy import and_, or_
 from ert_storage.database import Session, get_db
 from ert_storage import database_schema as ds
 
@@ -30,22 +30,23 @@ async def get_eclipse_summary_vectors(
         column_list: Optional[List[str]] = Query(None)
 
 ) -> Response:
-    #TODO - Preflight checks
-    print(column_list)
+    # TODO - Preflight checks
 
     ensemble = db.query(ds.Ensemble).filter_by(id=ensemble_id).one()
 
-    #Base record info filter
+    # Base record info filter
     filter =[ds.RecordInfo.ensemble_pk == ensemble.pk,
-             ds.RecordInfo.record_class==ds.RecordClass.response]
+             ds.RecordInfo.record_class == ds.RecordClass.response]
 
-    #Add column name filter 
+    # Add column name filter 
     if column_list is not None:
         # Maybe only use LIKE when wildcards are actually present
-        # And use = for explicit names
-        filter_group = [ ds.RecordInfo.name.like(x.replace("*","%").replace("?","_")) for x in column_list]
+        # And use = for explicit names to make resulting query more efficent
+        filter_group = [ds.RecordInfo.name.like(x.replace("*", "%")
+                                                .replace("?", "_"))
+                        for x in column_list]
         filter.append(or_(*filter_group))
-     
+
     records = (
         db.query(ds.Record)
         .filter(ds.Record.realization_index != None)
@@ -53,22 +54,21 @@ async def get_eclipse_summary_vectors(
         .filter(and_(*filter))
     ).all()
 
-    #May be more efficent to do this as part of query
+    # May be more efficent to do this as part of query
+    # TODO deal with frequency 
     df_list = []
     for record in records:
         labels = record.f64_matrix.labels
-        data={"DATE":labels[0],
-              "REAL":record.realization_index,
+        data={"DATE": labels[0],
+              "REAL": record.realization_index,
               record.record_info.name:record.f64_matrix.content[0]
         }
         data_df = pd.DataFrame(data)
-        df_list.append(data_df.set_index(["DATE","REAL"]))
-        
-    #What to return if no columns match??
-        
+        df_list.append(data_df.set_index(["DATE", "REAL"])
+                       )
+    # What to return if no columns match??
+
     return Response(
-        content=pd.concat(df_list, axis=1,join="outer").to_csv(),
+        content=pd.concat(df_list, axis=1, join="outer").to_csv(),
         media_type="text/csv",
     )
-
-    
